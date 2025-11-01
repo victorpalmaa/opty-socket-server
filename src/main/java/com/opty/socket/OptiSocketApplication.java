02231e6d7165d72e4f582407696c62f1ebe05906
Eduardo Kairalla <eduardokairalla123@gmail.com>
2025-11-01 11:43:38 -0300

    feat: add Spring Boot application entry point

---
+++ src/main/java/com/opty/socket/OptiSocketApplication.java
@@ -0,0 +1,31 @@
+/**
+ * Main entry point for Opty Socket Server.
+ * 
+ * @author Opty Development Team
+ * @version 0.0.0
+ */
+
+package com.opty.socket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+/**
+ * CODE
+ */
+
+
+@Slf4j
+@SpringBootApplication
+@EnableScheduling
+public class OptiSocketApplication {
+
+    public static void main(String[] args) {
+        log.info("Starting Opty Socket Server...");
+        SpringApplication.run(OptiSocketApplication.class, args);
+        log.info("Opty Socket Server started successfully!");
+    }
+}
